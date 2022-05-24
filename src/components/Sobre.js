67efbb60b223663ae49b5046ec5a09ee19225b9d
ISAIAS <cg3004121@LAB117M18.IFSP>
2022-05-24 16:11:37 -0300

    Ajuste no espaçamento do texto do sobre

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -5,11 +5,12 @@ function Sobre(){
     return(
         
         <div className="sobretext">
-            <h1>Sobre Nós</h1>
-            <p>Fundamos este negócio principalmente pelo amor que temos por animais, pois o seu bem-estar significa muito para nós.
+            <h1 className="title">Sobre Nós</h1>
+            <p className="um">Fundamos este negócio principalmente pelo amor que temos por animais, pois o seu bem-estar significa muito para nós.
             O negócio de suprimentos para animais de estimação tem crescido muito durante os últimos anos e achamos que seria bom se 
             os donos de animais tivessem uma loja onde pudessem achar tudo que precisam para manter seus bichinhos felizes e saudáveis num só lugar.
-            Queremos que a experiência de fazer compras na Happy Animals seja divertida para a família toda. Traga seu peludo e receba tratamento VIP
+           </p>
+           <p className="dois"> Queremos que a experiência de fazer compras na Happy Animals seja divertida para a família toda. Traga seu peludo e receba tratamento VIP
             durante suas compras. Teste nossos produtos, pegue um lanche para seu animal de estimação, escolha itens essenciais e divirta-se!</p>
         </div>
     
